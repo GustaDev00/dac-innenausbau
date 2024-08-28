2425c5e5d78e924c43ccfbb9825d5959a00b139b
Gustavo pontes <gustavo.4dev@gmail.com>
2024-08-28 07:53:53 -0300

    feat(theme): color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -13,6 +13,7 @@ export const theme = {
   metallicSilver: "#A6AFB5",
   gainsboro: "#DBDBDB",
   antiFlashWhite: "#F0F0F0",
+  davysGrey: "#545454",
 };
 
 // get the color name in https://www.color-name.com
