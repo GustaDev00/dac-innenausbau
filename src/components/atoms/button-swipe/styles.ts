6cb1f71f29071b30f2792d4dbdc238839fd1fe8c
Gustavo pontes <gustavo.4dev@gmail.com>
2024-09-02 18:22:23 -0300

    refactor: Add hover effect to mail and phone icons

--- src/components/atoms/button-swipe/styles.ts
+++ src/components/atoms/button-swipe/styles.ts
@@ -7,12 +7,19 @@ const swipeStyles = css`
   align-items: center;
   gap: 2.4rem;
   padding: 1rem 2.3rem 1rem 3.3rem;
-  background: ${({ theme }) => theme.carnelian};
+  background: linear-gradient(90deg, rgba(0, 0, 0, 1) 0%, rgba(174, 23, 23, 1) 65%);
+  background-size: 600%;
+  background-position: right;
   color: ${({ theme }) => theme.white};
   font-size: 1.6rem;
   font-weight: 400;
   line-height: 193.75%;
   letter-spacing: -0.0002rem;
+  transition: background-position 0.4s ease;
+
+  &:hover {
+    background-position: left;
+  }
 `;
 
 export const ButtonSwipe = styled.button`
--- src/components/atoms/phone/styles.ts
+++ src/components/atoms/phone/styles.ts
@@ -2,6 +2,12 @@ import styled from "styled-components";
 import { Phone as _Phone } from "@/components/svgs/phone";
 import { MailPhone } from "@/styles/components/mail-phone";
 
-export const Phone = styled(MailPhone)``;
+export const Phone = styled(MailPhone)`
+  transition: opacity 0.4s ease;
+
+  &:hover {
+    opacity: 0.6;
+  }
+`;
 
 export const Icon = styled(_Phone)``;
