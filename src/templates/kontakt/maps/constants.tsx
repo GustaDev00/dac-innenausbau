6b9003c92ecab9bb97114525aad228ceb1585fb9
Gustavo pontes <gustavo.4dev@gmail.com>
2024-09-12 10:02:49 -0300

    feat: build

--- src/templates/kontakt/maps/constants.tsx
+++ src/templates/kontakt/maps/constants.tsx
@@ -1,6 +1,10 @@
 export default {
   title: <>Unser Standort</>,
-  address: [<b>Dac Innenausbau GmbH</b>, <>Speerstrasse 8</>, <>9400 Rorschach</>],
+  address: [
+    <b key="1">Dac Innenausbau GmbH</b>,
+    <span key="2">Speerstrasse 8</span>,
+    <span key="3">9400 Rorschach</span>,
+  ],
   maps: {
     src: "imgs/kontakt/maps/maps.png",
     alt: "Karte",
